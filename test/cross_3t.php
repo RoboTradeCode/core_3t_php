c9062c196454119832b46e30d08ba3660692d536
kleninmaxim <maksim_klenin_99@mail.ru>
2022-04-11 17:58:57 +0300

    Generate initial data, separate orderbooks, balances, etc., which is taken from memcached

--- test/cross_3t.php
+++ test/cross_3t.php
@@ -1,7 +1,5 @@
 <?php
 
-use Src\Aeron;
-use Src\Api;
 use Src\Cross3T;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
@@ -9,24 +7,63 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 $memcached = new Memcached();
 $memcached->addServer('localhost', 11211);
 
-$cross_3t = new Cross3T();
-
-$publisher = new AeronPublisher("aeron:ipc");
-
-$api = new Api('binance', 'cross_3t_php', 'core', '1');
+//$cross_3t = new Cross3T();
 
 while (true) {
-
-    usleep(1000000);
-
-    // пример использования command to gate
-    $code = $publisher->offer($api->createOrder('BTC/USDT', 'limiit', 'buy', 0.001, 40000));
 
     sleep(1);
 
     $memcached_data = $memcached->getMulti($memcached->getAllKeys());
 
-    print_r(array_keys($memcached->getMulti($memcached->getAllKeys())));
+    print_r(array_keys($memcached_data));
+
+    if (isset($memcached_data['config'])) {
+
+        $config = $memcached_data['config'];
+
+        $memcached->delete('config');
+
+        unset($memcached_data['config']);
+
+        echo '[Ok] Config is update' . PHP_EOL;
+
+    }
+
+    foreach ($memcached_data as $key => $data)
+        if (isset($data)) {
+
+            $parts = explode('_', $key);
+
+            $exchange = $parts[0];
+            $action = $parts[1];
+            $value = $parts[2] ?? null;
+
+            if ($action == 'balances') {
+                $balances[$exchange] = $data;
+            } elseif ($action == 'orderbook' && $value) {
+                $orderbooks[$value][$exchange] = $data;
+            } else {
+                $undefined[$key] = $data;
+            }
+
+        }
+
+    if (isset($balances))
+        print_r($balances) . PHP_EOL;
+
+    if (isset($orderbooks))
+        print_r($orderbooks) . PHP_EOL;
+
+    if (isset($config))
+        print_r($config) . PHP_EOL;
+
+    if (isset($undefined)) {
+
+        echo '[WARNING] Config is update' . PHP_EOL;
+
+        print_r($undefined) . PHP_EOL;
+
+    }
 
     //$cross_3t->run($balances, $orderbooks, $rates, $server, $data['symbol']);
 
