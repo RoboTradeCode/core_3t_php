ed57861a2c33f8a2a4c415b14af05c1c1a9d5dd8
kleninmaxim <maksim_klenin_99@mail.ru>
2022-04-13 13:33:15 +0300

    Remove data

--- kernel/test_c.php
+++ kernel/test_c.php
@@ -21,38 +21,6 @@ while (true) {
     // берет все данные из memcached
     $memcached_data = $memcached->getMulti($memcached->getAllKeys());
 
-    $memcached_data = [
-        'binance_orderbook_BTC/USDT' => [
-            'bids' => [
-                [45793.94, 0.007352],
-                [45793.93, 0.972659],
-                [45793.91, 2.140652],
-                [45793.77, 0.000436],
-                [45793.76, 0.0261],
-            ],
-            'asks' => [
-                [46286.36, 0.26865],
-                [46286.37, 0.102385],
-                [46286.38, 0.018932],
-                [46354.34, 0.060668],
-                [46399.99, 1.503934],
-            ],
-            'symbol' => 'BTC/USDT',
-            'timestamp' => 1649070863000,
-        ],
-        'binance_order_1212412' => [
-            'id' => 1212412,
-            'timestamp' => 1644489501321977,
-            'status' => 'open',
-            'symbol' => 'BTC/USDT',
-            'type' => 'limit',
-            'side' => 'sell',
-            'price' => 40400.34,
-            'amount' => 0.00124,
-            'filled' => 0,
-        ],
-    ];
-
     foreach ($memcached_data as $key => $memcached_datum) {
 
         if ($memcached_datum) {
