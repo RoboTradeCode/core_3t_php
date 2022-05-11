6409937e55a0e24f391a80827500d4f1115f52ad
kleninmaxim <maksim_klenin_99@mail.ru>
2022-05-11 18:19:45 +0300

    Fix mistake

--- src/Main.php
+++ src/Main.php
@@ -91,7 +91,7 @@ class Main
 
         $html .= '<br /><br /> Balances: <pre>' . json_encode($balances, JSON_PRETTY_PRINT) . '</pre> <br /><br /> Orderbooks: <pre>' . json_encode($orderbooks, JSON_PRETTY_PRINT) . '</pre> <br /><br /> Best results: <pre>' . json_encode($best_result, JSON_PRETTY_PRINT) . '</pre> <br /><br /> Results: <pre>' . json_encode($results, JSON_PRETTY_PRINT) . '</pre>';
 
-        $index = fopen('/home/klenin/work/awesome/core/cache/test.html', 'w');
+        $index = fopen('/var/www/html/test.html', 'w');
 
         fwrite($index, $html);
 
