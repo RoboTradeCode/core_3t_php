4ac06ae2529ebc007b025275f282f25021929ad6
kleninmaxim <maksim_klenin_99@mail.ru>
2022-07-19 14:32:27 +0300

    Fix proof of config

--- src/Configurator.php
+++ src/Configurator.php
@@ -42,100 +42,12 @@ class Configurator
     {
 
         if (
-            !isset($cross_3t_php['exchange']) ||
-            !isset($cross_3t_php['exchanges']) ||
-            !isset($cross_3t_php['expired_orderbook_time']) ||
-            !isset($cross_3t_php['min_profit']) ||
-            !isset($cross_3t_php['min_deal_amounts']) ||
-            !isset($cross_3t_php['rates']) ||
-            !isset($cross_3t_php['max_deal_amounts']) ||
-            !isset($cross_3t_php['max_depth']) ||
-            !isset($cross_3t_php['fees']) ||
-            !isset($config_from_configurator['configs']['core_config']['aeron']) ||
-            !isset($config_from_configurator['configs']['core_config']['aeron']['publishers']['gate']) ||
-            !isset($config_from_configurator['configs']['core_config']['aeron']['publishers']['log']) ||
-            !isset($config_from_configurator['configs']['core_config']['aeron']['subscribers']['balance']['channel']) ||
-            !isset($config_from_configurator['configs']['core_config']['aeron']['subscribers']['balance']['destinations']) ||
-            !isset($config_from_configurator['configs']['core_config']['aeron']['subscribers']['balance']['stream_id']) ||
-            !isset($config_from_configurator['configs']['core_config']['aeron']['subscribers']['orderbooks']['channel']) ||
-            !isset($config_from_configurator['configs']['core_config']['aeron']['subscribers']['orderbooks']['destinations']) ||
-            !isset($config_from_configurator['configs']['core_config']['aeron']['subscribers']['orderbooks']['stream_id']) ||
-            !isset($config_from_configurator['configs']['core_config']['aeron']['subscribers']['orders']['channel']) ||
-            !isset($config_from_configurator['configs']['core_config']['aeron']['subscribers']['orders']['destinations']) ||
-            !isset($config_from_configurator['configs']['core_config']['aeron']['subscribers']['orders']['stream_id']) ||
             !isset($config_from_configurator['markets']) ||
             !isset($config_from_configurator['assets_labels']) ||
             !isset($config_from_configurator['routes'])
         ) {
 
             echo 'Wrong config get from configurator' . PHP_EOL;
-
-            echo 'Example config: 
-            "core_config":{
-                "cross_3t_php":{
-                    "exchange":"ftx",
-                    "exchanges":[
-                        "ftx"
-                    ],
-                    "expired_orderbook_time":500000,
-                    "min_profit":{
-                        "BTC":0,
-                        "ETH":0,
-                        "USDT":0
-                    },
-                    "min_deal_amounts":{
-                        "BTC":0,
-                        "ETH":0,
-                        "USDT":0
-                    },
-                    "rates":{
-                        "BTC":31000,
-                        "ETH":2300,
-                        "USDT":1
-                    },
-                    "max_deal_amounts":{
-                        "BTC":0.01,
-                        "ETH":0.1,
-                        "USDT":200
-                    },
-                    "max_depth":3,
-                    "fees":{
-                        "ftx":0.1
-                    }
-                },
-                "aeron":{
-                    "publishers":{
-                        "gate":{
-                            "channel":"aeron:udp?control=172.31.14.205:40456|control-mode=dynamic",
-                            "stream_id":1003
-                        },
-                        "log":{
-                            "channel":"aeron:udp?control=172.31.14.205:40456|control-mode=dynamic",
-                            "stream_id":1005
-                        }
-                    },
-                    "subscribers":{
-                        "balance":{
-                            "channel":"aeron:udp?control-mode=manual",
-                            "destinations":[
-                                "aeron:udp?endpoint=172.31.14.205:40461|control=172.31.14.205:40456"
-                            ],
-                            "stream_id":1002
-                        },
-                        "orderbooks":{
-                            "channel":"aeron:udp?control-mode=manual",
-                            "destinations":[
-                                "aeron:udp?endpoint=172.31.14.205:40458|control=172.31.14.205:40456",
-                                "aeron:udp?endpoint=172.31.14.205:40459|control=18.159.92.185:40456",
-                                "aeron:udp?endpoint=172.31.14.205:40460|control=54.248.171.18:40456"
-                            ],
-                            "stream_id":1001
-                        }
-                    }
-                }
-            }
-            ';
-            echo PHP_EOL;
 
             print_r($cross_3t_php);
             print_r($config_from_configurator);
