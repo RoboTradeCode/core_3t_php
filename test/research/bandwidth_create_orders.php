4d4cce62ec0c31974ef12c27199ad69afc4abeb3
kleninmaxim <maksim_klenin_99@mail.ru>
2022-08-11 17:19:53 +0300

    Time low

--- test/research/bandwidth_create_orders.php
+++ test/research/bandwidth_create_orders.php
@@ -114,9 +114,9 @@ if (isset($precisions)) {
 
     echo PHP_EOL . 'Create Orders [END]----------------------------------------------------------------------------------' . PHP_EOL;
 
-    echo '[' . date('Y-m-d H:i:s') . '] [WAIT] 30 seconds' . PHP_EOL;
+    echo '[' . date('Y-m-d H:i:s') . '] [WAIT] 10 seconds' . PHP_EOL;
 
-    sleep(30);
+    sleep(10);
 
     // берем реальные ордера
     do {
