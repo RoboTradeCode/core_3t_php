6b254fb3d74b10118c9ec394e6cb57c0b542c203
kleninmaxim <maksim_klenin_99@mail.ru>
2022-09-20 17:22:02 +0300

    Fix bug by default property

--- src/M3BestPlace/M3BestPlace.php
+++ src/M3BestPlace/M3BestPlace.php
@@ -18,7 +18,7 @@ class M3BestPlace extends Main
     private array $markets;
     private string $main_exchange;
     private string $delta_exchange;
-    private array $profits;
+    private array $profits = [];
     private array $expired_orders = [];
 
     public function __construct(int $max_depth, array $rates, array $max_deal_amounts, array $fees, array $markets, string $main_exchange, string $delta_exchange = '', array $options = [])
