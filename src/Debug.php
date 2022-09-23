9afecfe66a45a27efeb05e6f6e7d29ee6660a441
kleninmaxim <maksim_klenin_99@mail.ru>
2022-09-23 12:57:21 +0300

    Fix bug 10

--- src/Debug.php
+++ src/Debug.php
@@ -13,16 +13,16 @@ class Debug
 
     public static function simplePrint(array $array, string $head_message): void
     {
-        echo PHP_EOL . $head_message . ' [START]----------------------------------------------------------------------------------' . PHP_EOL;
+        echo $head_message . ' [START]----------------------------------------------------------------------------------' . PHP_EOL;
 
         foreach ($array as $key => $arr) echo '[' . date('Y-m-d H:i:s') . '] ' . $key . ' ' . $arr . PHP_EOL;
 
-        echo $head_message . ' [END]------------------------------------------------------------------------------------' . PHP_EOL . PHP_EOL;
+        echo $head_message . ' [END]------------------------------------------------------------------------------------' . PHP_EOL;
     }
 
     public static function printOrders(array $orders, string $exchange): void
     {
-        echo PHP_EOL . 'Orders: ' . $exchange . ' [START]----------------------------------------------------------------------------------' . PHP_EOL;
+        echo 'Orders: ' . $exchange . ' [START]----------------------------------------------------------------------------------' . PHP_EOL;
 
         foreach ($orders as $order) {
             $message = '[' . date('Y-m-d H:i:s') . '] Client Order Id ' . $order['client_order_id'] . ' Price ' . $order['price'] . ' Side ' . $order['side'] . ' Amount: ' . ($order['amount'] ?? 'null');
@@ -32,16 +32,16 @@ class Debug
             echo $message . PHP_EOL;
         }
 
-        echo 'Orders: ' . $exchange . ' [END]------------------------------------------------------------------------------------' . PHP_EOL . PHP_EOL;
+        echo 'Orders: ' . $exchange . ' [END]------------------------------------------------------------------------------------' . PHP_EOL;
     }
 
     public static function printBalances(array $balances): void
     {
-        echo PHP_EOL . 'Balances [START]---------------------------------------------------------------------------------' . PHP_EOL;
+        echo 'Balances [START]---------------------------------------------------------------------------------' . PHP_EOL;
 
         foreach ($balances as $asset => $balance)
             echo '[' . date('Y-m-d H:i:s') . '] ' . $asset . ' (free: ' . $balance['free'] . ' | used: ' . $balance['used'] . ' | total: ' . $balance['total'] . ') ' . PHP_EOL;
 
-        echo 'Balances [END]----------------------------------------------------------------------------------' . PHP_EOL . PHP_EOL;
+        echo 'Balances [END]----------------------------------------------------------------------------------' . PHP_EOL;
     }
 }
